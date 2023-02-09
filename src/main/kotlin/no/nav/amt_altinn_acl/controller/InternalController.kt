3f2e79b35f047a96301ef22b9423b66b6e5032fd
Izis Frøyhov <izis.froyhov@nav.no>
2023-02-09 10:10:23 +0100

    Gjør det mulig å trigge sync av altinn

--- src/main/kotlin/no/nav/amt_altinn_acl/controller/InternalController.kt
+++ src/main/kotlin/no/nav/amt_altinn_acl/controller/InternalController.kt
@@ -3,6 +3,7 @@ package no.nav.amt_altinn_acl.controller
 import jakarta.servlet.http.HttpServletRequest
 import no.nav.amt_altinn_acl.client.altinn.AltinnClient
 import no.nav.amt_altinn_acl.client.altinn.AltinnRettighet
+import no.nav.amt_altinn_acl.jobs.AltinnUpdater
 import no.nav.amt_altinn_acl.utils.SecureLog.secureLog
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/internal")
 class InternalController (
-	private val altinnClient: AltinnClient
+	private val altinnClient: AltinnClient,
+	private val altinnUpdater: AltinnUpdater
 ) {
 	@Unprotected
 	@GetMapping("/altinn/organisasjoner")
@@ -44,6 +46,20 @@ class InternalController (
 			return altinnClient.hentRettigheter(norskIdent = fnr, orgNr)
 		}
 		secureLog.error("Attempted external access to /altinn/rettigheter")
+		throw RuntimeException("No access")
+	}
+
+	@Unprotected
+	@GetMapping("/altinn/synkroniser")
+	fun synkroniserAltinnRettigheter(
+		servlet: HttpServletRequest,
+	) : List<AltinnRettighet> {
+		secureLog.info("Reached /altinn/synkroniser")
+		if (isInternal(servlet)) {
+			secureLog.info("Passed internal /altinn/synkroniser")
+			altinnUpdater.update()
+		}
+		secureLog.error("Attempted external access to /altinn/synkroniser")
 		throw RuntimeException("No access")
 	}
 
