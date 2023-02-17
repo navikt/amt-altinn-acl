7daf9559a7d52e620a25c3573966c9ff7c78bcd0
Magnus Evensberget <magnus.evensberget@nav.no>
2023-02-17 10:36:13 +0100

    Refakturering - Fjernet mer bruke av right jeg ikke så

--- src/main/kotlin/no/nav/amt_altinn_acl/controller/RolleController.kt
+++ src/main/kotlin/no/nav/amt_altinn_acl/controller/RolleController.kt
@@ -24,7 +24,7 @@ class RolleController(
 
 		val roller = rolleService.getRollerForPerson(norskIdent)
 		return HentRollerResponse(
-			roller.map { right -> HentRollerResponse.TiltaksarrangorRoller(right.organizationNumber, right.roller.map { it.rolleType }) }
+			roller.map { rolle -> HentRollerResponse.TiltaksarrangorRoller(rolle.organizationNumber, rolle.roller.map { it.rolleType }) }
 		)
 	}
 
