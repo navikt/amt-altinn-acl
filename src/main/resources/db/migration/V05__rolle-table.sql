3dcc62cf30b87500a997b18c0654f4bdbc77cfda
Magnus Evensberget <magnus.evensberget@nav.no>
2023-02-17 10:49:48 +0100

    Refakturering - splittet tabellgenereing, la til index på norsk_ident

--- src/main/resources/db/migration/V05__rolle-table.sql
+++ src/main/resources/db/migration/V05__rolle-table.sql
@@ -1,11 +1,3 @@
-CREATE TABLE person
-(
-    id                BIGSERIAL PRIMARY KEY,
-    norsk_ident       VARCHAR                  NOT NULL UNIQUE,
-    created           TIMESTAMP WITH TIME ZONE NOT NULL default current_timestamp,
-    last_synchronized TIMESTAMP WITH TIME ZONE NOT NULL default 'epoch'
-);
-
 CREATE TABLE rolle
 (
     id                  BIGSERIAL PRIMARY KEY,
