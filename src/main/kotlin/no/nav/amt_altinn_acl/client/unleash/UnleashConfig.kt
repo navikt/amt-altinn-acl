fddfcbc05f24feb6f17846f846ea1464356c3608
Tormod Fossum <tormod.fossum@nav.no>
2025-01-21 15:02:15 +0100

    Unngå navnekonflikt for bønner

--- src/main/kotlin/no/nav/amt_altinn_acl/client/unleash/UnleashConfig.kt
+++ src/main/kotlin/no/nav/amt_altinn_acl/client/unleash/UnleashConfig.kt
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile
 @Configuration
 class UnleashConfig {
 	@Bean
-	fun unleashClient(
+	fun defaultUnleash(
 		@Value("\${app.env.unleashUrl}") unleashUrl: String,
 		@Value("\${app.env.unleashApiToken}") unleashApiToken: String,
 	): DefaultUnleash {
