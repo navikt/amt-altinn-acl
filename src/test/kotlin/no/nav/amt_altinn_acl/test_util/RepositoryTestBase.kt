0cee4f3bfb68ab0efd13779c1d041361bf6e5056
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-07-14 19:52:42 +0200

    SingletonPostgresContainer.kt gir ingen merverdi i testene, fjerner derfor denne

--- src/test/kotlin/no/nav/amt_altinn_acl/test_util/RepositoryTestBase.kt
+++ src/test/kotlin/no/nav/amt_altinn_acl/test_util/RepositoryTestBase.kt
@@ -1,11 +1,13 @@
 package no.nav.amt_altinn_acl.test_util
 
+import no.nav.amt_altinn_acl.test_util.DbTestDataUtils.cleanDatabase
 import org.junit.jupiter.api.AfterEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestConstructor
+import org.testcontainers.containers.PostgreSQLContainer
 import javax.sql.DataSource
 
 @ActiveProfiles("test")
@@ -16,11 +18,13 @@ abstract class RepositoryTestBase {
 	private lateinit var dataSource: DataSource
 
 	@AfterEach
-	fun cleanDatabase() = DbTestDataUtils.cleanDatabase(dataSource)
+	fun cleanDatabase() = cleanDatabase(dataSource)
 
 	companion object {
+		private const val POSTGRES_DOCKER_IMAGE_NAME = "postgres:14-alpine"
+
 		@ServiceConnection
 		@Suppress("unused")
-		private val container = SingletonPostgresContainer.postgresContainer
+		private val container = PostgreSQLContainer<Nothing>(POSTGRES_DOCKER_IMAGE_NAME)
 	}
 }
