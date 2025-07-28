f397b20f9d503a50c16bc2172bec5fdd69e2ce64
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2025-07-28 10:38:35 +0200

    Med okhttp:5 er body non-null (#308)

--- src/main/kotlin/no/nav/amt_altinn_acl/utils/JsonUtils.kt
+++ src/main/kotlin/no/nav/amt_altinn_acl/utils/JsonUtils.kt
@@ -13,7 +13,5 @@ object JsonUtils {
 		.registerModule(JavaTimeModule())
 		.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-	inline fun <reified T> fromJsonString(jsonStr: String): T {
-		return objectMapper.readValue(jsonStr)
-	}
+	inline fun <reified T : Any> fromJsonString(jsonStr: String): T = objectMapper.readValue(jsonStr)
 }
