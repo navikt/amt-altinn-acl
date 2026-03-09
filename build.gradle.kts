f2bb7f5493f4779faa8d4f5ad64515ff45fc037a
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-03-09 11:47:21 +0100

    JDK 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     val kotlinVersion = "2.3.10"
 
@@ -74,8 +76,9 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(24)
+    jvmToolchain(25)
     compilerOptions {
+        jvmTarget = JvmTarget.JVM_25
         freeCompilerArgs.addAll(
             "-Xjsr305=strict",
             "-Xannotation-default-target=param-property",
